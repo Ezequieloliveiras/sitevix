bdfd5422913076bbf7d4904654dffa4b9182972e
ezequiel oliveira <ezequieloliveiraes@outlook.com>
2024-04-23 12:59:30 -0300

    ajustando section teo

--- src/components/sectionTwo/SectionTwo.js
+++ src/components/sectionTwo/SectionTwo.js
@@ -11,8 +11,6 @@ import {
   StyledImage,
 } from '../HomeStyles'
 
-import CardCorporate from'./CardCorporate'
-
 export default function SectionTwo() {
 
 
@@ -30,28 +28,17 @@ export default function SectionTwo() {
           </StyledTitleSection>
 
           <Grid container spacing={3} justifyContent="center" >
-<<<<<<< HEAD
-            <Grid item xs={12} sm={5}>
-=======
 
             <Grid item xs={12} sm={4}>
               <StyledBoxTitle>
->>>>>>> fd0335541bcb2d1f7c9731c310c9afa068bc2039
 
                 <StyledSubtitle >
 
                   Sites Corporativos
 
-<<<<<<< HEAD
-              </StyledSubtitle>
-            <CardCorporate />
-
-              {/* <StyledParagraphy>
-=======
                 </StyledSubtitle>
 
                 <StyledParagraphy>
->>>>>>> fd0335541bcb2d1f7c9731c310c9afa068bc2039
 
                   Destaque a sua marca e alcance um público global com um site corporativo profissional.
 
@@ -62,8 +49,6 @@ export default function SectionTwo() {
               <StyledBoxImage>
 
                 <StyledImage src='https://i.imgur.com/9ogyyKs.png' alt="Corporativo" />
-<<<<<<< HEAD
-=======
 
               </StyledBoxImage>
 
@@ -88,61 +73,22 @@ export default function SectionTwo() {
               <StyledBoxImage>
 
                 <StyledImage src='https://i.imgur.com/Gw0oBSf.png' alt="Lojas Online" />
->>>>>>> fd0335541bcb2d1f7c9731c310c9afa068bc2039
 
-              </StyledBoxImage> */}
+              </StyledBoxImage>
 
             </Grid>
 
             <Grid item xs={12} sm={4}>
-<<<<<<< HEAD
-               <StyledSubtitle>
-=======
               <StyledBoxTitle>
                 <StyledSubtitle>
 
                   Portfólios de Arte
->>>>>>> fd0335541bcb2d1f7c9731c310c9afa068bc2039
 
                 </StyledSubtitle>
 
-<<<<<<< HEAD
-              </StyledSubtitle>
-            <CardCorporate />
-
-              {/* <StyledParagraphy>
-
-                Venda seus produtos e serviços 24 horas por dia, 7 dias por semana, com uma loja online personalizada.
-
-              </StyledParagraphy>
-
-              <StyledBoxImage>
-
-                <StyledImage src='https://i.imgur.com/Gw0oBSf.png' alt="Lojas Online" />
-
-              </StyledBoxImage>  */}
-
-            </Grid>
-
-            <Grid item xs={12} sm={4}>
-
-              <StyledSubtitle>
-
-                Portfólios de Arte
-
-              </StyledSubtitle>
-              <CardCorporate />
-{/* 
-              <StyledParagraphy>
-
-                Exiba seu trabalho de forma deslumbrante e conquiste novos clientes com um portfólio de arte online.
-
-              </StyledParagraphy>
-=======
                 <StyledParagraphy>
 
                   Exiba seu trabalho de forma deslumbrante e conquiste novos clientes com um portfólio de arte online.
->>>>>>> fd0335541bcb2d1f7c9731c310c9afa068bc2039
 
                 </StyledParagraphy>
               </StyledBoxTitle>
@@ -150,7 +96,7 @@ export default function SectionTwo() {
 
                 <StyledImage src='https://i.imgur.com/wuxDWOn.png' alt="Portfólios de Arte" />
 
-              </StyledBoxImage> */}
+              </StyledBoxImage>
 
             </Grid>
 
