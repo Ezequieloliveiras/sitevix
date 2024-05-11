0ace02445402adaafd08dd6d6486722fe1fba5ff
ezequiel oliveira <ezequieloliveiraes@outlook.com>
2024-05-11 14:16:48 -0300

    testando carousel

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -1,80 +1,69 @@
-// import { register } from 'swiper/element/bundle'
-// import { Swiper, SwiperSlide } from 'swiper/react'
+
 import Carousel from 'react-material-ui-carousel'
 import Image from 'next/image'
 
-// import 'swiper/css'
-// import 'swiper/css/pagination'
-// import 'swiper/css/scrollbar'
-// import 'swiper/css/autoplay'
-// import './carousel.css'
+
 
 import SitesCorporativos from '../../../public/images/sitescorporativos.avif'
 import Estetica from '../../../public/images/estetica.avif'
 import LojaOnline from '../../../public/images/lojaonline.avif'
-import { Box, Card, CardMedia } from '@mui/material'
+import { Box } from '@mui/material'
+import styled from 'styled-components'
 
-// register()
+
+const StyledBox = styled(Box)`
+height: 500px;
+@media (max-width:768px) {
+    height: 300px;
+  }
+`
+
+const CardSlide = styled.div`
+height: 500px ;
+  @media (max-width:768px) {
+    height: 300px;
+  }
+`
+
+
 
 function SlideCarousel() {
 
-  // const data = [
 
-  //   { id: '3', image: LojaOnline, title: 'Blogs' },
-  //   { id: '2', image: Estetica, title: 'Lojas Online' },
-  //   { id: '1', image: SitesCorporativos, title: 'Sites Corporativos' },
-
-  // ]
 
   return (
 
     <div className='container'>
 
 
-      <Box sx={{ height: '500px' }}>
+      <StyledBox >
         <Carousel autoPlay>
 
-          <div style={{ height: '500px' }}>
+          <CardSlide >
 
-            <Image src={SitesCorporativos} style={{ width: '100%', height: '100%', objectFit: 'cover' }} alt='sitescorporativos' />
+            <Image src={SitesCorporativos}
+             style={{ width: '100%', height: '100%', objectFit: 'cover' }} alt='sitescorporativos' />
 
-          </div>
+          </CardSlide>
 
-          <div style={{ height: '500px' }}>
+          <CardSlide >
 
-            <Image src={Estetica} style={{ width: '100%', height: '100%', objectFit: 'cover' }} alt='estetica' />
+            <Image src={Estetica} 
+            style={{ width: '100%', height: '100%', objectFit: 'cover' }} alt='estetica' />
 
-          </div>
+          </CardSlide>
 
-          <div style={{ height: '500px' }}>
+          <CardSlide >
 
-            <Image src={LojaOnline} style={{ width: '100%', height: '100%', objectFit: 'cover' }} alt='lojaonline' />
+            <Image src={LojaOnline} 
+            style={{ width: '100%', height: '100%', objectFit: 'cover' }} alt='lojaonline' />
 
-          </div>
+          </CardSlide>
 
         </Carousel>
 
-      </Box>
+      </StyledBox>
 
-
-      {/* 
-        <Swiper 
-          className='swiper-slider'
-          slidesPerView={1}
-          pagination={{clickable: true}}
-          autoplay={true}
-          >
-          {data.map((item) => (
-
-            <SwiperSlide key={item.id} className='swiper-slider'>
-
-                <Image src={item.image} alt='sites' className='slider-item' fetchPriority='high'/>
-
-            </SwiperSlide>
-
-          ))}
-
-        </Swiper> */}
 
     </div>
 
