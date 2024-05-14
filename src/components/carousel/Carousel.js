89ef8a0d79f25b0bbb912e30ee684d264fa6a5c3
ezequiel oliveira <ezequieloliveiraes@outlook.com>
2024-05-13 23:21:24 -0300

    retirando slider das telas mobile

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -1,95 +1,63 @@
+import { useState, useEffect } from 'react';
+import { register } from 'swiper/element/bundle';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import 'swiper/css/autoplay';
+import './carousel.css';
+import SitesCorporativos from '../../../public/images/sitescorporativos.avif';
+import Estetica from '../../../public/images/estetica.avif';
+import LojaOnline from '../../../public/images/lojaonline.avif';
+import Image from 'next/image';
 
-import { register } from 'swiper/element/bundle'
-import { Swiper, SwiperSlide } from 'swiper/react'
-// import Carousel from 'react-material-ui-carousel'
-
-
-import 'swiper/css'
- import 'swiper/css/pagination'
- import 'swiper/css/scrollbar'
- import 'swiper/css/autoplay'
- import './carousel.css'
-
-import SitesCorporativos from '../../../public/images/sitescorporativos.avif'
- import Estetica from '../../../public/images/estetica.avif'
-import LojaOnline from '../../../public/images/lojaonline.avif'
-import Image from 'next/image'
-// import { Box, Card, CardMedia } from '@mui/material'
-
-register()
+register();
 
 function SlideCarousel() {
+  const [isDesktop, setIsDesktop] = useState(true);
 
-   const data = [
+  useEffect(() => {
+    const handleResize = () => {
+      setIsDesktop(window.innerWidth >= 768);
+    };
 
+    handleResize(); // Verifica o tamanho da tela quando o componente é montado
+
+    // Adiciona um event listener para detectar alterações no tamanho da tela
+    window.addEventListener('resize', handleResize);
+
+    // Remove o event listener quando o componente é desmontado
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const data = [
     { id: '1', image: SitesCorporativos, title: 'Sites Corporativos' },
     { id: '3', image: LojaOnline, title: 'Blogs' },
     { id: '2', image: Estetica, title: 'Lojas Online' },
+  ];
 
-   ]
+  if (!isDesktop) {
+    return null; // Não renderiza o swiper em telas menores que 768 pixels
+  }
 
   return (
-
     <div className='container'>
-      
-        <Swiper 
-          className='swiper-slider'
-          slidesPerView={1}
-          pagination={{clickable: true}}
-          autoplay={true}
-          >
-          {data.map((item) => (
-
-            <SwiperSlide key={item.id} className='swiper-slider'>
-
-                <Image src={item.image} alt='sites' className='slider-item' fetchPriority='high' priority/>
-
-            </SwiperSlide>
-
-          ))}
-
-        </Swiper> 
-
+      <Swiper
+        className='swiper-slider'
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        autoplay={true}
+      >
+        {data.map((item) => (
+          <SwiperSlide key={item.id} className='swiper-slider'>
+            <Image src={item.image} alt='sites' className='slider-item' fetchPriority='high' priority />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
-
-  )
+  );
 }
 
-export default SlideCarousel
-
-
-// import SitesCorporativos from '../../../public/images/sitescorporativos.avif'
-// import Estetica from '../../../public/images/estetica.avif'
-// import LojaOnline from '../../../public/images/lojaonline.avif'
-
-// import {
-//   StyledBox,
-//   CarouselSlide,
-//   CardSlide,
-//   ImageTeste,
-// } from './StylesCarousel'
-
-// function SlideCarousel() {
-//   return (
-//     <div className='container'>
-//       <StyledBox>
-//         <CarouselSlide autoPlay>
-//           <CardSlide>
-//             <ImageTeste src={Estetica} priority
-//               alt='estetica' />
-//           </CardSlide>
-//           <CardSlide>
-//             <ImageTeste src={SitesCorporativos} priority
-//               alt='empresa' />
-//           </CardSlide>
-//           <CardSlide>
-//             <ImageTeste src={LojaOnline} priority
-//               alt='lojaonline' />
-//           </CardSlide>
-//         </CarouselSlide>
-//       </StyledBox>
-//     </div>
-//   )
-// }
-
-// export default SlideCarousel
+export default SlideCarousel;
