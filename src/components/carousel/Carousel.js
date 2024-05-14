df93515f8f2dd2456be4cb36ee02ad51d94714c7
ezequiel oliveira <ezequieloliveiraes@outlook.com>
2024-05-13 22:39:07 -0300

    trocando carousel material ui pelo swipper

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -1,35 +1,96 @@
-import SitesCorporativos from '../../../public/images/sitescorporativos.avif'
-import Estetica from '../../../public/images/estetica.avif'
-import LojaOnline from '../../../public/images/lojaonline.avif'
 
-import {
-  StyledBox,
-  CarouselSlide,
-  CardSlide,
-  ImageTeste,
-} from './StylesCarousel'
+import { register } from 'swiper/element/bundle'
+import { Swiper, SwiperSlide } from 'swiper/react'
+// import Carousel from 'react-material-ui-carousel'
+
+
+import 'swiper/css'
+ import 'swiper/css/pagination'
+ import 'swiper/css/scrollbar'
+ import 'swiper/css/autoplay'
+ import './carousel.css'
+
+import SitesCorporativos from '../../../public/images/sitescorporativos.avif'
+ import Estetica from '../../../public/images/estetica.avif'
+import LojaOnline from '../../../public/images/lojaonline.avif'
+import Image from 'next/image'
+// import { Box, Card, CardMedia } from '@mui/material'
+
+register()
 
 function SlideCarousel() {
+
+   const data = [
+
+    { id: '3', image: LojaOnline, title: 'Blogs' },
+    { id: '2', image: Estetica, title: 'Lojas Online' },
+    { id: '1', image: SitesCorporativos, title: 'Sites Corporativos' },
+
+   ]
+
   return (
+
     <div className='container'>
-      <StyledBox>
-        <CarouselSlide autoPlay>
-          <CardSlide>
-            <ImageTeste src={Estetica} priority
-              alt='estetica' />
-          </CardSlide>
-          <CardSlide>
-            <ImageTeste src={SitesCorporativos} priority
-              alt='empresa' />
-          </CardSlide>
-          <CardSlide>
-            <ImageTeste src={LojaOnline} priority
-              alt='lojaonline' />
-          </CardSlide>
-        </CarouselSlide>
-      </StyledBox>
+
+      
+        <Swiper 
+          className='swiper-slider'
+          slidesPerView={1}
+          pagination={{clickable: true}}
+          autoplay={true}
+          >
+          {data.map((item) => (
+
+            <SwiperSlide key={item.id} className='swiper-slider'>
+
+                <Image src={item.image} alt='sites' className='slider-item' fetchPriority='high'/>
+
+            </SwiperSlide>
+
+          ))}
+
+        </Swiper> 
+
     </div>
+
   )
 }
 
 export default SlideCarousel
+
+
+// import SitesCorporativos from '../../../public/images/sitescorporativos.avif'
+// import Estetica from '../../../public/images/estetica.avif'
+// import LojaOnline from '../../../public/images/lojaonline.avif'
+
+// import {
+//   StyledBox,
+//   CarouselSlide,
+//   CardSlide,
+//   ImageTeste,
+// } from './StylesCarousel'
+
+// function SlideCarousel() {
+//   return (
+//     <div className='container'>
+//       <StyledBox>
+//         <CarouselSlide autoPlay>
+//           <CardSlide>
+//             <ImageTeste src={Estetica} priority
+//               alt='estetica' />
+//           </CardSlide>
+//           <CardSlide>
+//             <ImageTeste src={SitesCorporativos} priority
+//               alt='empresa' />
+//           </CardSlide>
+//           <CardSlide>
+//             <ImageTeste src={LojaOnline} priority
+//               alt='lojaonline' />
+//           </CardSlide>
+//         </CarouselSlide>
+//       </StyledBox>
+//     </div>
+//   )
+// }
+
+// export default SlideCarousel
